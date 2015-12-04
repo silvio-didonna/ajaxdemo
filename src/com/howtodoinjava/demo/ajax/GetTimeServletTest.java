98fb2429d2a092efa017bdbcbcbf582dec76db52
Silvio Didonna <tnw513@gmail.com>
2015-12-04 23:18:49 +0100

    test junit

---
+++ src/com/howtodoinjava/demo/ajax/GetTimeServletTest.java
@@ -0,0 +1,15 @@
+package com.howtodoinjava.demo.ajax;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GetTimeServletTest {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		assertEquals(0, 0);
+	}
+
+}
